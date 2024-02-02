2bc3f45536d270d2e3e00bfe632780e8830b8f59
Kevin Song <kevinsong@Kevins-MacBook-Pro.local>
2024-02-02 15:27:38 -0500

    First commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Things to do</h1>
+      <p>Workout!</p>
+    </div>
+  );
+}
+
+export default Note;
